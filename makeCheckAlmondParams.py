4cafeb4f7bb1f2593d133259fdc39b32a877e1b2
johnthomas22 <johnthomas22@users.noreply.github.com>
2023-01-21 16:28:18 +0000

    Create makeCheckAlmondParams.py

---
+++ makeCheckAlmondParams.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+email_address_from = input("Email address from: ")
+email_address_to = input("Email address to: ")
+email_password = input("Gmail App password: ")
+parameters = {
+    'email_address_from': email_address_from,
+    'email_address_to': [email_address_from,email_address_to],
+    'email_password': email_password
+  }
+
+import pickle
+import stat
+import os
+file_name = '~/checkAlmond.pickled'
+
+with open(file_name, 'wb') as out_file:
+  pickle.dump(parameters, out_file)
+  st = os.stat(file_name)
+  os.chmod(file_name, st.st_mode )
